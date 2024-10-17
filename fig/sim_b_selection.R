d2a21815496190144a24c715291268cdefa88823
Logan Harris <lharris4211998@gmail.com>
2024-10-17 13:54:49 -0500

    New bias visual

--- fig/sim_b_selection.R
+++ fig/sim_b_selection.R
@@ -6,183 +6,46 @@ pal <- c("#31543B", "#48628A", "#94AA3D", "#7F9CB1", "#D9D1BE", "#3E3C3A")
 params <- list(seed = 1234, iterations = 1000,
                simulation_function = "gen_data_abn", simulation_arguments = list(
                  n = 100, p = 100, a = 1, b = 1, rho = 0.5,
-                 beta = c(2, rep(0, 99))
+                 beta = 2
                ), script_name = "b_selection")
 res <- indexr::read_objects(rds_path, params)
 
-## Avg B vars in bootstrapping
+
+add_bias <- res$orig_est - apply(res$ests, 1, mean)
+hist(add_bias)
+
+add_b_bias <- res$orig_corrs*-res$orig_bs - apply(res$corrs*-res$bs, 1, mean)
+hist(add_b_bias)
+
+add_n_bias <- res$orig_an_bias - apply(res$an_bias, 1, mean)
+hist(add_n_bias)
+
+add_err_bias <- res$orig_err_bias - apply(res$err_bias, 1, mean)
+hist(add_err_bias)
+
+sum_bias <- add_b_bias + add_n_bias + add_err_bias
+
+data <- data.frame(
+  add_bias = add_bias,
+  add_b_bias = add_b_bias,
+  add_n_bias = add_n_bias,
+  add_err_bias = add_err_bias,
+  sum_bias = sum_bias
+)
+
+# Reshape data into long format for ggplot2
+long_data <- pivot_longer(data, cols = everything(), names_to = "variable", values_to = "value") %>%
+  mutate(variable = factor(variable, 
+                           levels = c("add_bias", "add_b_bias", "add_n_bias", "add_err_bias", "sum_bias"),
+                           labels = c("Additional Bias", "Attributable to B", "Attributable to N", "Attr. to Err Corr (Irreducible)", "Sum Attributable")))
+
+# Create density plots using ggplot2
 pdf("./fig/sim_b_selection.pdf", height = 5, width = 7)
-data.frame(
-  avg_bs = apply(res$bs, 1, mean),
-  iter = 1:1000
-) %>%
-  ggplot(aes(x = iter, ymin = 0, ymax = avg_bs)) +
-  geom_errorbar() +
-  geom_hline(yintercept = mean(res$orig_bs), color = "red") +
-  ylab("Avg B Beta Est") + xlab("") +
-  theme_bw()
-dev.off()
-
-## Correlations vs bias
-## First histogram (res$ests)
-which_sim <- which.max(apply(res$bs, 1, mean) - res$orig_bs)
-df <- data.frame(
-  ests = res$ests[which_sim, ],
-  corrs = res$corrs[which_sim, ],
-  bs = res$bs[which_sim, ]
-)
-df_vline <- data.frame(
-  ests = res$orig_est[which_sim],
-  corrs = res$orig_corrs[which_sim],
-  bs = res$orig_bs[which_sim],
-  LineType = "Original Estimate"
-)
-p1 <- ggplot(df, aes(x = ests)) +
-  geom_histogram(binwidth = diff(range(df$ests)) / 30, 
-                 fill = pal[1], color = "white") +
-  geom_vline(data = df_vline, aes(xintercept = ests, color = LineType),
-             linetype = "dashed", size = 1) +
-  scale_color_manual(name = "", values = c("Original Estimate" = "red")) +
-  labs(
-    title = paste("Bootstrapped Debiased A Variable Estimates - Simulation", which_sim),
-    x = "Estimates",
-    y = "Frequency"
-  ) +
-  theme_minimal()
-
-## Second histogram (res$corrs)
-p2 <- ggplot(df, aes(x = corrs)) +
-  geom_histogram(binwidth = diff(range(df$corrs)) / 30, 
-                 fill = pal[2], color = "white") +
-  geom_vline(data = df_vline, aes(xintercept = corrs, color = LineType),
-             linetype = "dashed", size = 1) +
-  scale_color_manual(name = "", values = c("Original Estimate" = "red")) +
-  labs(
-    title = paste("Bootstrapped AB Variable Correlations - Simulation", which_sim),
-    x = "Correlations",
-    y = "Frequency"
-  ) +
-  theme_minimal()
-
-## Third histogram (res$bs)
-p3 <- ggplot(df, aes(x = bs)) +
-  geom_histogram(binwidth = diff(range(df$bs)) / 30, 
-                 fill = pal[3], color = "white") +
-  geom_vline(data = df_vline, aes(xintercept = bs, color = LineType),
-             linetype = "dashed", size = 1) +
-  scale_color_manual(name = "", values = c("Original Estimate" = "red")) +
-  labs(
-    title = paste("Bootstrapped B Variable Estimates - Simulation", which_sim),
-    x = "bs",
-    y = "Frequency"
-  ) +
-  theme_minimal()
-
-# Combine the plots vertically with a shared legend
-pdf("./fig/sim_b_selection_single.pdf", height = 7, width = 7)
-(p1 / p2 / p3) +
-  plot_layout(guides = "collect") &
-  theme(legend.position = "bottom")
-dev.off()
-
-
-# Using sapply to extract slopes across simulations
-corr_bias_rel <- sapply(1:1000, function(which_sim) {
-  x <- res$corrs[which_sim, ] - res$orig_corrs[which_sim]
-  y <- res$orig_est[which_sim] - res$ests[which_sim, ]
-  fit <- lm(y ~ x)
-  coef(fit)["x"]
-})
-
- # ggtitle("Slope estimates for regression of boot debiased estimate vs corr")
-pdf("./fig/sim_b_selection_corr_slopes.pdf", height = 7, width = 7)
-ggplot(data.frame(x = corr_bias_rel), aes(x = x)) +
-  geom_histogram(color = "white", fill = pal[3], binwidth = 0.05) +
-  theme_bw() + ylab("") +
-  xlab("") 
-dev.off()
-
-pdf("./fig/sim_b_selection_single_corr_bias_est.pdf", height = 5, width = 7)
-data.frame(
-  corr = res$corrs[which_sim, ] - res$orig_corrs[which_sim],
-  diff_est = res$orig_est[which_sim] - res$ests[which_sim, ],
-  b_diffs = res$bs[which_sim,] - res$orig_bs[which_sim]
-) %>%
-  ggplot(aes(x = corr, y = diff_est)) +
-  geom_vline(aes(xintercept = 0), alpha = 0.5) +
-  geom_hline(aes(yintercept = 0), alpha = 0.5) +
-  geom_point(aes(color = b_diffs)) +
-  labs(
-    title = paste("Simulation", which_sim),
-    x = "Additional Correlation",
-    y = "Additional Bias",
-    color = "Additional B est"
-  ) +
+ggplot(long_data, aes(x = value, fill = variable, color = variable)) +
+  geom_density(alpha = 0.2) +
   theme_bw() +
-  scale_color_viridis_b()
-dev.off()
-
-## Is a greater B est correlated with a higher correlation between A and B?
-## Annotations
-bdiffs <- res$bs[which_sim,] - res$orig_bs[which_sim]
-aabbg <- mean(bdiffs[res$corrs[which_sim, ] > res$orig_corrs[which_sim]])
-aabbl <- mean(bdiffs[res$corrs[which_sim, ] < res$orig_corrs[which_sim]])
-
-## Plot
-pdf("./fig/sim_b_selection_single_corr_bias.pdf", height = 5, width = 7)
-data.frame(
-  corr = res$corrs[which_sim, ],
-  diff_est = res$orig_est[which_sim] - res$ests[which_sim, ],
-  b_diffs = res$bs[which_sim,] - res$orig_bs[which_sim]
-) %>%
-  ggplot(aes(x = corr, y = b_diffs)) +
-  geom_hline(aes(yintercept = 0), alpha = 0.5) +
-  geom_point(alpha = 0.2) +
-  theme_bw() +
-  geom_vline(aes(xintercept = res$orig_corrs[which_sim]), color = "red") +
   labs(
-    title = paste("Simulation", which_sim),
-    x = "Correlation",
-    y = "Additional B Bias"
-  ) + 
-  annotate("text", x = res$orig_corrs[which_sim] + 0.1, y = aabbg, label = glue("Average Additional B Bias: {round(aabbg, 3)}")) +
-  annotate("text", x = res$orig_corrs[which_sim] - 0.1, y = aabbl, label = glue("Average Additional B Bias: {round(aabbl, 3)}"))
+    title = "Additional Bootstrap Bias Compared to Original Debiased Est (Decomp)",
+    x = "Bias", y = "Density", color = "Bias Type", fill = "Bias Type"
+  )
 dev.off()
-
-###################################
-########## All Simulations ########
-###################################
-aabb_diff <- sapply(1:1000, function(which_sim) {
-  
-  bdiffs <- res$bs[which_sim,]
-  aabbg <- mean(bdiffs[res$corrs[which_sim, ] > res$orig_corrs[which_sim]])
-  aabbl <- mean(bdiffs[res$corrs[which_sim, ] < res$orig_corrs[which_sim]]) 
-  aabbg - aabbl
-  
-})
-pdf("./fig/sim_b_selection_corr_bias.pdf", height = 5, width = 7)
-ggplot(data.frame(x = aabb_diff), aes(x = x)) +
-  geom_histogram(binwidth = 0.001, color = "white", fill = pal[3]) +
-  theme_bw() + xlab("") + ylab("") 
-dev.off()
-  ## ggtitle("Diff avg. B est between boot draws with cor < and > the orig sample corr between AB")
-
-# addtl_bias <- res$orig_est - apply(res$ests, 1, mean)
-# addtl_bias_est <- apply(res$bs * res$corrs, 1, mean) - res$orig_bs * res$orig_corrs
-# plot(addtl_bias_est, addtl_bias)
-
-
-## Attempt 2
-bs_quant <- sapply(1:1000, function(which_sim) {
-  sum(res$bs[which_sim,] > res$orig_bs[which_sim]) /  sum(res$bs[which_sim,] != res$orig_bs[which_sim])
-})
-
-hist(bs_quant,
-     xlab = "Proportion draws",
-     main = "Prop. boot draws for B greater than original estimate")
-abline(v = 0.5, col = "red")
-
-## breaking down mean / median bias
-brks <- seq(-.05, .10, by = 0.01)
-hist(res$orig_est - apply(res$ests, 1, mean), breaks = brks)
-hist(apply((res$corrs * res$bs) - (res$orig_corrs * res$orig_bs), 1, mean), breaks = brks)
